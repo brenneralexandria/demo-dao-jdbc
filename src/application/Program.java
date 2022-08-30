08de7d695232f545bff047b955c068f393f1859b
Brenner Alexandria <obrenneralexandria@gmail.com>
2022-08-30 20:32:02 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +13,8 @@ public class Program {
 		Department obj = new Department(1, "books");
 		
 		Seller seller = new Seller(21, "bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
 		
 		System.out.println(seller);
